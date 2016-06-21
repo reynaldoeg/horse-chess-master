80ad6cdc0b756c906411b05efe9e8df5812e91a2
Reynaldo <reynaldoeg_esparza@hotmail.com>
2016-06-20 23:51:20 -0500

    Cronometro js

---
+++ js/autoplay.js
@@ -0,0 +1,45 @@
+var secs;
+var mins;
+var cronometer;
+
+autoplay();
+
+function autoplay(){
+
+	ResetTime();
+	StarTime();
+
+}
+
+function StarTime(){
+
+	secs = 0;
+	mins = 0;
+
+	s = document.getElementById("seconds");
+	m = document.getElementById("minutes");
+
+	cronometer = setInterval(function(){
+
+		if( secs==60 ){
+			secs = 0;
+			mins++;
+			if(mins<10) m.innerHTML = "0"+mins;
+			else m.innerHTML = mins;
+
+			if(mins==60) mins=0;
+		}
+
+		if(secs<10) s.innerHTML = "0"+secs;
+		else s.innerHTML = secs;
+		
+		secs++;
+		
+	},1000);
+	
+
+}
+
+function ResetTime(){
+	clearInterval(cronometer);
+}
